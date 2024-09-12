96bf874142b2b38310f038dbfecda50789444707
Rory Collins <rory.collins@hotmail.co.uk>
2024-09-12 13:37:29 +0100

    more display tweaks

--- src/Chart.tsx
+++ src/Chart.tsx
@@ -8,17 +8,26 @@ import {
     Legend,
     ResponsiveContainer,
     ReferenceLine,
+    CartesianGrid,
 } from 'recharts';
 import { SimulationResults } from './interfaces';
 
-const yAxisFormatter = (n : number) => {
-    if(n > 1_000_000) return "£" + n/1_000_000 + "m";
-    else if(n > 1_000) return "£" + n/1_000 + "k";
+const roundTo = (n: number, p: number) => {
+    return Math.round(n/p) * p
+}
+
+
+const numericFormatter = (n: number | number[]) : string => {
+    if(Array.isArray(n)){
+        return numericFormatter(n[0]) + " - " + numericFormatter(n[1]);
+    }
+    if (n > 1_000_000) return "£" + roundTo(n/1_000_000, 0.1)  + "m";
+    else if (n > 1_000) return "£" + roundTo(n/1_000, 1) + "k";
     else return "£" + n
 }
 
-export default function Chart({chartData}: {chartData: SimulationResults}) {
-    const data = chartData.annualData.map((x:any) => { return { age: x.age, percentiles: [x.percentile10, x.percentile90], median: x.median } })
+export default function Chart({ chartData }: { chartData: SimulationResults }) {
+    const data = chartData.annualData.map((x: any) => { return { age: x.age, percentiles: [x.percentile10, x.percentile90], median: x.median } })
     return (
         <div>
             <ResponsiveContainer width="100%" height={400}>
@@ -27,11 +36,12 @@ export default function Chart({chartData}: {chartData: SimulationResults}) {
                     height={400}
                     margin={{
                         left: 200,
-                        right:200,
+                        right: 200,
                     }}
                     data={data}>
-                    <XAxis dataKey="age" name="Age"/>
-                    <YAxis tickFormatter={yAxisFormatter}/>
+                    <CartesianGrid strokeDasharray="3 3" />
+                    <XAxis dataKey="age" name="Age" />
+                    <YAxis tickFormatter={numericFormatter} />
                     <Area
                         type="monotone"
                         dataKey="percentiles"
@@ -44,10 +54,7 @@ export default function Chart({chartData}: {chartData: SimulationResults}) {
                     <Line type="natural" dataKey="median" stroke="#ff00ff" dot={false} connectNulls />
                     <ReferenceLine x={chartData.medianRetirementAge} stroke='blue' width={2} />
                     <Legend />
-
-                    
-                    
-                    <Tooltip/>
+                    <Tooltip formatter={numericFormatter} />
                 </ComposedChart>
             </ResponsiveContainer>
         </div>
