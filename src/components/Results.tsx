bc6349959ab4ea608618eaa392f0260cd30f0519
Rory Collins <rory.collins@hotmail.co.uk>
2024-10-09 11:56:44 +0100

    Add favicon and fix styling for results cards

--- src/components/Results.tsx
+++ src/components/Results.tsx
@@ -1,36 +1,51 @@
 import { SimulationResults } from "../interfaces";
-import { Card, CardContent, Container } from "@mui/material";
+import { Card, CardContent, Container, createTheme, ThemeProvider } from "@mui/material";
 import Grid from "@mui/material/Grid2";
 import Chart from "./Chart";
-import "./Results.css"
+
+const theme = createTheme({
+    components: {
+        MuiCard: {
+            styleOverrides: {
+                root: {
+                    backgroundColor: "#edf1f5",
+                    height: 190,
+                    width: 180
+                }
+            }
+        }
+    }
+})
 
 export default function Results({ results }: { results?: SimulationResults }) {
     if (results === undefined) { return <></> }
     return (
         <Container>
-            <Grid container
-                spacing={3}
-                alignItems={"center"}
-                justifyContent="center">
-                <Card sx={{ width: 180, height: 190 }} >
-                    <CardContent>
-                        <h2>Success<br/>Rate</h2><br />
-                        <b>{(results.successRate * 100).toFixed(1)}%</b>
-                    </CardContent>
-                </Card>
-                <Card sx={{ width: 180, height: 190 }}>
-                    <CardContent>
-                        <h2>Annual Drawdown</h2><br />
-                        <b>£{(results.drawdownAtRetirement).toFixed(2)}</b>
-                    </CardContent>
-                </Card>
-                <Card sx={{ width: 180, height: 190 }}>
-                    <CardContent>
-                        <h2>Retirement Age</h2><br />
-                        <b>{results.medianRetirementAge}</b>
-                    </CardContent>
-                </Card>
-            </Grid>
+            <ThemeProvider theme={theme}>
+                <Grid container
+                    spacing={3}
+                    alignItems={"center"}
+                    justifyContent="center">
+                    <Card>
+                        <CardContent>
+                            <h2>Success<br />Rate</h2><br />
+                            <b>{(results.successRate * 100).toFixed(1)}%</b>
+                        </CardContent>
+                    </Card>
+                    <Card>
+                        <CardContent>
+                            <h2>Annual Drawdown</h2><br />
+                            <b>£{(results.drawdownAtRetirement).toFixed(2)}</b>
+                        </CardContent>
+                    </Card>
+                    <Card>
+                        <CardContent>
+                            <h2>Retirement Age</h2><br />
+                            <b>{results.medianRetirementAge}</b>
+                        </CardContent>
+                    </Card>
+                </Grid>
+            </ThemeProvider>
             <br />
             <Chart chartData={results} />
         </Container>
