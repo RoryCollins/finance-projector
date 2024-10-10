c64513b423a0c1b8da138e835d1b4118e65b193c
Rory Collins <rory.collins@hotmail.co.uk>
2024-10-10 11:21:51 +0100

    WIP defer retirement for up to three years if portfolio cannot handle it

--- src/RetirementCalculator.ts
+++ src/RetirementCalculator.ts
@@ -1,16 +1,22 @@
-import { EARLY_PENSION_AGE, STATE_PENSION_AGE } from "./constants";
+import { EARLY_PENSION_AGE, SAFE_WITHDRAWAL_RATE, STATE_PENSION_AGE } from "./constants";
 import { QueryDetails } from "./interfaces";
 import { PortfolioState } from "./SimulationRunner";
+
+const safeWithdrawalAmount = (portfolioValue: number): number => {
+    return portfolioValue * SAFE_WITHDRAWAL_RATE;
+}
 
 export class RetirementCalculator {
     private targetAge: number
     private targetDrawdown: number
     private deferInCrash: boolean
+    private deferUntilSwr: boolean
 
     constructor({ targetAge, targetDrawdown, deferInCrash }: QueryDetails) {
         this.targetAge = targetAge;
         this.targetDrawdown = targetDrawdown;
         this.deferInCrash = deferInCrash;
+        this.deferUntilSwr = true;
     }
 
     updateRetirementState = (state: PortfolioState): PortfolioState => {
@@ -21,8 +27,13 @@ export class RetirementCalculator {
             return { ...state, retired: true };
         }
         if (state.age >= this.targetAge) {
-            if (this.deferInCrash && state.interest < 1 && state.deferredRetirementCounter < 3) {
+            if (this.deferInCrash && state.deferredRetirementCounter && state.interest < 1) {
                 return { ...state, deferredRetirementCounter: state.deferredRetirementCounter + 1 }
+            }
+            if (this.deferUntilSwr && safeWithdrawalAmount(state.isaValue + state.pensionValue) < this.targetDrawdown) {
+                return state.deferredRetirementCounter < 3
+                    ? { ...state, deferredRetirementCounter: state.deferredRetirementCounter + 1 }
+                    : { ...state, retired: true, annualDrawdown: safeWithdrawalAmount(state.isaValue + state.pensionValue)}
             }
             if (state.isaValue >= ((EARLY_PENSION_AGE - state.age) * this.targetDrawdown)) {
                 return { ...state, retired: true };
